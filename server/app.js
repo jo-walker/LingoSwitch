e5e0f35fe1f5d7165efb62ad134f80b172e5b33c
Jo Tamir <bsnlkhm@gmail.com>
2024-07-17 18:28:17 -0400

    eslint-disable-next-line was added to keep standard fcn signature err, req, res, next

--- server/app.js
+++ server/app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const stringRoutes = require('./routes/strings');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+// Use Routes
 app.use('/api/strings', stringRoutes);
 
 // Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Something went wrong!' });
-  });
+// eslint-disable-next-line no-unused-vars
+  app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
 
-  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- server/routes/strings.js
+++ server/routes/strings.js
@@ -4,6 +4,5 @@ const stringController = require('../controllers/stringController');
 
 router.get('/', stringController.getStrings);
 router.post('/', stringController.addString);
-// Add routes for update, delete etc.
 
 module.exports = router;
